9acce50d1ef80cbf1989b996b7848b3e1621efa3
hashika <m.g.hashikamaduranga@gmail.com>
2019-11-12 23:46:45 +0530

    add js file to change more quickly

---
+++ app.js
@@ -0,0 +1,19 @@
+
+function visiblePage(){
+    document.getElementById('body').innerHTML=`
+<table>
+<tr><td style="color: white;font-size: 40px;padding-bottom: 50px">Projects</td></tr>
+<tr><th>Online Shopping Site</th></tr>
+<tr style="text-align: center"><td>php</td></tr>
+<tr style="text-align: center"><td>JavaScript</td></tr>
+<tr><th >Cricket App</th></tr>
+<tr style="text-align: center"><td>Java</td></tr>
+<tr><th>Daily Activity App</th></tr>
+<tr style="text-align: center"><td>laravel</td></tr>
+<tr style="text-align: center"><td>bootstrap</td></tr>
+<tr><th>User Management System</th></tr>
+<tr style="text-align: center"><td>php</td></tr>
+
+
+</table>`
+}
