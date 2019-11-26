c3c236acab068cccbe17df3c61ad153146ec806e
hashika <m.g.hashikamaduranga@gmail.com>
2019-11-26 15:27:09 +0530

    set details using js file

--- app.js
+++ app.js
@@ -2,18 +2,77 @@
 function visiblePage(){
     document.getElementById('body').innerHTML=`
 <table>
-<tr><td style="color: white;font-size: 40px;padding-bottom: 50px">Projects</td></tr>
-<tr><th>Online Shopping Site</th></tr>
-<tr style="text-align: center"><td>php</td></tr>
+<tr><td style="color: white;font-size: 40px;padding-bottom: 50px;">Projects</td></tr>
+<tr><th style="background-color:darkgrey">Online Shopping Site</th></tr>
+<tr style="text-align: center;"><td>php</td></tr>
 <tr style="text-align: center"><td>JavaScript</td></tr>
-<tr><th >Cricket App</th></tr>
+<tr><th style="background-color:darkgrey" >Cricket App</th></tr>
 <tr style="text-align: center"><td>Java</td></tr>
-<tr><th>Daily Activity App</th></tr>
+<tr><th style="background-color:darkgrey">Daily Activity App</th></tr>
 <tr style="text-align: center"><td>laravel</td></tr>
 <tr style="text-align: center"><td>bootstrap</td></tr>
-<tr><th>User Management System</th></tr>
+<tr><th style="background-color:darkgrey">User Management System</th></tr>
 <tr style="text-align: center"><td>php</td></tr>
 
 
 </table>`
+}
+
+function setContact(){
+    document.getElementById('body').innerHTML=`
+    <div class="contact">
+    <table>
+         <tr><td style="color: white;font-size: 40px;padding-bottom: 50px">Contacts</td></tr>
+        <th>Email</th>
+        <tr></tr>
+        <tr><td>m.g.hashikamaduranga@gmail.com</td></tr>
+        <tr><td>m.g.hashika96@gmail.com</td></tr>
+        <tr></tr>
+        <tr><th>Phone</th></tr>
+        <tr></tr>
+        <tr><td>0774164979</td></tr>
+        <tr><td>0770231782</td></tr>
+    </table>
+    
+        
+   
+    
+</div>
+
+    `
+}
+const Index=()=>{
+
+    document.getElementById('body').innerHTML=`
+    <div class="about">
+                    
+    <table>
+       
+        <th>About</th>
+        <tr><td>Email</td><td>m.g.hashikamaduranga@gmail.com</td></tr>
+        <tr><td>Mobile</td><td>0774164979</td></tr>
+        <tr><td>Date of Birth</td><td>1996-08-13</td></tr>
+        <tr><td>Address</td><td>BentotaKanda.Thalgampala,Galle,Sri Lanka</td></tr>
+        <tr></tr>
+        <tr><td style="color: aliceblue;font-size:20px"></td></tr>
+        
+    </table>
+   
+</div>
+
+<div class="content">
+                  <table><tr><td>Hello! I'm Hashika Maduranga,Studing <span style="color: rgb(229, 243, 124)">Software Engineering</span> undergraduater at University of Kelaniya. I can be able to design interesting projects which allow me to utilize and improve my skills. </td></tr></table>
+                      
+                  </div>
+              <div class="logo">
+                  <ul>
+                      <li><img src="images/angular.png" width="80px" height="80px"></li>
+                      <li><img src="images/flutter-logo.png" width="80px" height="80px"></li>
+                      <li><img src="images/python.jpg" width="80px" height="80px"></li>
+                      <li><img src="images/PHP-logo.png" width="80px" height="80px"></li>
+                      <li><img src="images/Javascript.jpg" width="80px" height="80px"></li>
+                      <li><img src="images/java-logo.png" width="80px" height="80px"></li>
+                  </ul>
+              </div> 
+    `
 }
