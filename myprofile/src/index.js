db976df2d8f36fc7a2c610e407bfc3fb61d6ba06
hashika <m.g.hashikamaduranga@gmail.com>
2020-01-11 22:44:23 +0530

    add some features to app

--- myprofile/src/index.js
+++ myprofile/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Home from './home';
 
 const App=()=>{
     return(
-        <h1>hashika</h1>
+       <div className="ui placeholder segment">
+           <div className="ui two column very relaxed stackable grid">
+               <div className="column">
+                    <Home/>
+               </div>
+               <div className="middle aligned column" >
+                    content
+               </div>
+           </div>
+       </div>
     )
 }
 ReactDom.render(<App/>,
