a1b4459a6f3ead82282703f34516cad26cbaf56d
hashika <m.g.hashikamaduranga@gmail.com>
2020-01-11 21:27:28 +0530

    update site using react

---
+++ myprofile/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App=()=>{
+    return(
+        <h1>hashika</h1>
+    )
+}
+ReactDom.render(<App/>,
+    document.querySelector('#root')
+)
