5dc1d0ad27d080563e6066ee849f0a4cc4516861
hashika <m.g.hashikamaduranga@gmail.com>
2020-01-12 23:13:17 +0530

    change some ui form

--- myprofile/src/index.js
+++ myprofile/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Home from './home';
+import SocialMedia from './socialMedia';
 
 const App=()=>{
     return(
-       <div className="ui placeholder segment">
+       <div className="ui segment">
            <div className="ui two column very relaxed stackable grid">
                <div className="column">
                     <Home/>
                </div>
-               <div className="middle aligned column" >
+               <div className=" middle aligned column" >
+                    <SocialMedia/>
                     content
                </div>
            </div>
---
+++ myprofile/src/socialMedia.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const SocialMedia=()=>{
+    return(
+        <div className="row">
+            <button class="ui facebook button">
+            <i class="facebook icon"></i>
+            Facebook
+            </button>
+            <button class="ui twitter button">
+            <i class="twitter icon"></i>
+            Twitter
+            </button>
+            <button class="ui google plus button">
+            <i class="google plus icon"></i>
+            Google Plus
+            </button>
+            <button class="ui linkedin button">
+            <i class="linkedin icon"></i>
+            LinkedIn
+            </button>
+            <button class="ui instagram button">
+            <i class="instagram icon"></i>
+            Instagram
+            </button>
+            <button class="ui youtube button">
+            <i class="youtube icon"></i>
+            YouTube
+            </button>
+        </div>
+    )
+}
+export default SocialMedia;
